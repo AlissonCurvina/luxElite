0f219caaa333cdc31ec1f84c5e25b00bec5cdd11
EvillyCosta <evillynascicosta@gmail.com>
2024-09-26 17:31:44 -0300

    mensagens implementadas em login, cadastro e esqueci

--- src/app/views/forget-password/forget-password.component.ts
+++ src/app/views/forget-password/forget-password.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; // Import FormsModule para o [(ngModel)]
 
 @Component({
   selector: 'app-forget-password',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './forget-password.component.html',
   styleUrl: './forget-password.component.css'
 })
 export class ForgetPasswordComponent {
 
+  email: string = ''; // variável para armazenar o email inserido
+  successMessage: string | null = null; // variável para armazenar a mensagem de sucesso
+
+  // função chamada ao clicar em "Enviar Link"
+  onSendLink() {
+    if (this.email) { 
+      // simulação do envio do email e exibição da mensagem de sucesso
+      this.successMessage = 'Email enviado com sucesso!';
+    } else {
+      this.successMessage = 'Por favor, insira um email válido.';
+    }
+
+    // adicionar a lógica real de envio de link para recuperação de senha
+  }
 }
--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -2,15 +2,23 @@ import { Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateNewUserComponent } from '../create-new-user/create-new-user.component';
 import { ForgetPasswordComponent } from '../forget-password/forget-password.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  successMessage: string | null = null;
+
+  onLogin() {
+    // pegar logica do prof na aula
+    // aqui você pode adicionar lógica para autenticar o usuário
+    this.successMessage = 'Login efetuado com sucesso!';
+  }
 
 }
 
