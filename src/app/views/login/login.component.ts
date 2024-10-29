63a2fe382b86165a36ee5096d829fa0e86a30ea6
Evilly Costa <evillynascicosta@gmail.com>
2024-10-28 22:40:05 -0300

    validaçao login e cadastro teste

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -3,25 +3,47 @@ import { RouterModule, Routes } from '@angular/router';
 import { CreateNewUserComponent } from '../create-new-user/create-new-user.component';
 import { ForgetPasswordComponent } from '../forget-password/forget-password.component';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
   successMessage: string | null = null;
+  errorMessage: any;
+  email: any;
+  password: any;
 
   onLogin() {
-    // pegar logica do prof na aula
-    // aqui você pode adicionar lógica para autenticar o usuário
-    this.successMessage = 'Login efetuado com sucesso!';
+    //verificação se os campos estão vazios
+    if (!this.email || !this.password) {
+      this.errorMessage = 'Insira um login válido';
+      this.successMessage = null;
+      return;
+    }
+
+    //verificação do formato do email
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; //expressão regular para validar email
+    if (!emailPattern.test(this.email)) {
+      this.errorMessage = 'Insira um email válido';
+      this.successMessage = null;
+      return;
+    }
+
+    //lógica de login (simulação)
+    if (this.email === 'evillynascicosta@gmail.com' && this.password === '123456') {
+      this.successMessage = 'Login efetuado com sucesso!';
+      this.errorMessage = null;
+    } else {
+      this.errorMessage = 'Credenciais inválidas. Tente novamente.';
+      this.successMessage = null;
+    }
   }
-
 }
-
 const routes: Routes = [
   { path: 'create', component: CreateNewUserComponent},
   { path: 'forget-password', component: ForgetPasswordComponent}
